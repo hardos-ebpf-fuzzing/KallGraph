c2d3798940e0faa0a40579be44ea03d9e14eaf2a
RubinDu <rd25@illinois.edu>
2025-09-28 21:22:58 -0500

    Added caching to fromSVFTypeToLLVMType

--- src/include/Port.hpp
+++ src/include/Port.hpp
@@ -6,6 +6,11 @@
 #include <llvm/Support/Casting.h>
 #include "SVF-LLVM/LLVMModule.h"
 #include "SVFIR/SVFValue.h"
+
+#include "llvm/ADT/DenseMap.h"
+#include <shared_mutex>
+#include <unordered_map>
+
 // static inline const Function *fromSVFFuncToLLVMFunc(const SVFFunction *svfFunc) {
 //     // SVFFunction is a subclass of SVFValue;
 //     // llvm::Function is a subclass of llvm::Value
@@ -29,8 +34,34 @@ static inline const T *fromSVFValueToLLVMValue(const SVF::SVFValue *svfValue) {
     return llvm::dyn_cast<T>(SVF::LLVMModuleSet::getLLVMModuleSet()->getLLVMValue(svfValue));
 }
 
-static inline const llvm::Type *fromSVFTypeToLLVMType(const SVF::SVFType *svfType) {
-    return SVF::LLVMModuleSet::getLLVMModuleSet()->getLLVMType(svfType);
+static inline const llvm::Type*
+fromSVFTypeToLLVMType(const SVF::SVFType* svfType) noexcept {
+  if (!svfType) return nullptr;
+
+  thread_local std::unordered_map<const SVF::SVFType*, const llvm::Type*> tlCache;
+  if (auto it = tlCache.find(svfType); it != tlCache.end())
+    return it->second;
+
+  static llvm::DenseMap<const SVF::SVFType*, const llvm::Type*> gCache;
+  static std::shared_mutex gCacheMutex;
+
+  {
+    std::shared_lock rlk(gCacheMutex);
+    if (auto it = gCache.find(svfType); it != gCache.end()) {
+      tlCache.emplace(svfType, it->second);
+      return it->second;
+    }
+  }
+
+  const auto* ms = SVF::LLVMModuleSet::getLLVMModuleSet();
+  const llvm::Type* ty = ms->getLLVMType(svfType);
+
+  {
+    std::unique_lock wlk(gCacheMutex);
+    auto [it, _] = gCache.try_emplace(svfType, ty);
+    tlCache.emplace(svfType, it->second);
+    return it->second;
+  }
 }
 
 static inline const SVF::SVFType *fromLLVMTypeToSVFType(const llvm::Type *llvmType) {
